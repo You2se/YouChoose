ccdfa2374d4483fe42e61cf12857470f36927cf1
Chris Lopez <pagarizabal2@gmail.com>
2018-10-11 22:21:09 +0200

    form now uses material-ui
    edited user schema to add support for genres

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,65 +1,129 @@
 // auth/Signup.js
-import React, { Component } from 'react';
-import AuthService from './AuthService'
+import React, { Component } from "react";
+import AuthService from "./AuthService";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
 export default class Signup extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { username: '', password: '' };
+    this.state = {
+      username: "",
+      password: "",
+      genres: {
+        actions: true,
+        drama: false,
+        comedy: false
+      }
+    };
     this.service = new AuthService();
   }
-    
-  handleFormSubmit = (event) => {
+
+  handleFormSubmit = event => {
     event.preventDefault();
     const username = this.state.username;
     const password = this.state.password;
 
-    this.service.signup(username, password)
-    .then( response => {
+    this.service
+      .signup(username, password)
+      .then(response => {
         this.setState({
-            username: "", 
-            password: "",
-            error: false
+          username: "",
+          password: "",
+          error: false
         });
-        this.props.getUser(response.user)
-    })
-    .catch( error => {
-      this.setState({
-        username: username,
-        password: password,
-        error: true
+        this.props.getUser(response.user);
+      })
+      .catch(error => {
+        this.setState({
+          username: username,
+          password: password,
+          error: true
+        });
       });
-    
-    })
-  }
+  };
 
-  handleChange = (event) => {  
-    const {name, value} = event.target;
-    this.setState({[name]: value});
-  }
-      
+  handleChange = event => {
+    //this.setState({ [name]: event.target.checked });
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
   render() {
-    return(
+    const { actions, drama, comedy } = this.state;
+    return (
       <div>
         <h3>Welcome!, create your account next:</h3>
+        <br />
+        <br />
+        <FormControl component="fieldset" className="form-control">
+          <FormLabel component="legend">Username</FormLabel>
+          <TextField
+            name="username"
+            hintText="Username"
+            floatingLabelText="username"
+            value={this.state.username}
+            onChange={e => this.handleChange(e)}
+            floatingLabelFixed
+          />
+          <FormLabel component="legend">Password</FormLabel>
+          <TextField
+            name="password"
+            hintText="Password"
+            floatingLabelText="password"
+            value={this.state.password}
+            onChange={e => this.handleChange(e)}
+            floatingLabelFixed
+          />
 
-        <form onSubmit={this.handleFormSubmit}>
-          <fieldset>
-            <label>Username:</label>
-            <input type="text" name="username" value={this.state.username} onChange={ e => this.handleChange(e)}/>
-          </fieldset>
-          
-          <fieldset>
-            <label>Password:</label>
-            <input type="password" name="password" value={this.state.password} onChange={ e => this.handleChange(e)} />
-          </fieldset>
-          
-          <input type="submit" value="Sign up" />
-        </form>
-        <h1>{this.state.error ? 'Error': ''}</h1>
+          <br />
+          <br />
+          <FormGroup>
+            <FormLabel component="legend">Select Genres</FormLabel>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={actions}
+                  //onChange={this.handleChange('gilad')}
+                  value="actions"
+                />
+              }
+              label="Actions"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={drama}
+                  //onChange={this.handleChange('jason')}
+                  value="drama"
+                />
+              }
+              label="Drama"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={comedy}
+                  //onChange={this.handleChange('antoine')}
+                  value="comedy"
+                />
+              }
+              label="Comedy"
+            />
+          </FormGroup>
 
+          <Button onClick={this.handleFormSubmit} primary>
+            Submit
+          </Button>
+        </FormControl>
+
+        <h1>{this.state.error ? "Error" : ""}</h1>
       </div>
-    )
+    );
   }
 }
