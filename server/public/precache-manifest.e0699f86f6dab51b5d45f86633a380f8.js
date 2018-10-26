3442a61e3dcda4581ada5b872d3439b8f27b9290
TeoDiaz <tdiazsan@gmail.com>
2018-10-26 15:39:11 +0200

    build files

---
+++ server/public/precache-manifest.e0699f86f6dab51b5d45f86633a380f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c38bc18275944589404ad89d6540bf9e",
+    "url": "/static/media/Sofa.c38bc182.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d3016262f460067b7f1",
+    "url": "/static/js/main.9d301626.chunk.js"
+  },
+  {
+    "revision": "ae82ece9ce2c83fdc102",
+    "url": "/static/js/1.ae82ece9.chunk.js"
+  },
+  {
+    "revision": "9d3016262f460067b7f1",
+    "url": "/static/css/main.125bfe58.chunk.css"
+  },
+  {
+    "revision": "f9c21f6b5582dbdda1713fe8021765dd",
+    "url": "/index.html"
+  }
+];
